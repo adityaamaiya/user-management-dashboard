36b259d6b8a24538959916785b37620d1955548e
ADITYA AMAIYA <amaiyaaditya@gmail.com>
2025-01-21 10:57:42 +0530

    added components, services, and pages folder with setting up api.js and added placeholder files in folders

---
+++ src/pages/AddUser.js
@@ -0,0 +1,13 @@
+import React from "react";
+import UserForm from "../components/UserForm";
+import Navbar from "../components/Navbar";
+
+export default function AddUser() {
+  return (
+    <div>
+      <Navbar />
+
+      <UserForm />
+    </div>
+  );
+}
